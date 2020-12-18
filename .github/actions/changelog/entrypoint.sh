1af9d903d5a3831968adba14fe37aa242b8a7400
Svetlana Maltseva <5437289+leo-ri@users.noreply.github.com>
2020-12-18 20:12:41 +0300

    fix linter

--- .github/actions/changelog/entrypoint.sh
+++ .github/actions/changelog/entrypoint.sh
@@ -5,7 +5,7 @@
 echo "Commits: " > changelog.txt
 #find previos tag
 start_tag=$(git for-each-ref refs/tags/ --count=2 --sort=-version:refname --format='%(refname:short)' | awk 'NR==2')
-if [[ -n "${start_tag}" ]]; then
+if [ -n "${start_tag}" ]; then
     git log ${start_tag}...${{ github.ref }} --pretty=format:"- %s" >> changelog.txt
 else
     git log --pretty=format:"- %s" >> changelog.txt # first tag
