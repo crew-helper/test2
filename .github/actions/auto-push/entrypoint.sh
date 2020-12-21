2dd764add8eb5cd262798cc9bfe36905935bf995
Svetlana Maltseva <5437289+leo-ri@users.noreply.github.com>
2020-12-21 22:05:47 +0300

    commit

--- .github/actions/auto-push/entrypoint.sh
+++ .github/actions/auto-push/entrypoint.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-gpg2 -k
-gpg2 --help
-
 git config --local user.email "crew.helper@yahoo.com"
 git config --local user.name "Crew Helper"
-git
+
 
 # git checkout -b "${version}" #version tag
-git add *all-in-one*
-git status
-git commit -m "Update configuration"
+# git add *all-in-one*
+# git status
+# git commit -m "Update configuration"
 # git push origin ${version} --force
 # git push origin main --force
 git push origin sign
