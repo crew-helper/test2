78d2509641604938b9361023bd663dfb276b6c53
Svetlana Maltseva <5437289+leo-ri@users.noreply.github.com>
2020-12-21 22:52:31 +0300

    update

---
+++ .github/actions/commit-sign/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+git config --local user.email "crew.helper@yahoo.com"
+git config --local user.name "Crew Helper"
+
+
+# git checkout -b "${version}" #version tag
+# git add *all-in-one*
+# git status
+# git commit -m "Update configuration"
+# git push origin ${version} --force
+# git push origin main --force
+git push origin sign
+# git tag "v${INPUT_VERSION}"
+# git push origin --tags
