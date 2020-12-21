897cc2bf71a4f17a0751330fb2afe2eb8765b5e0
Svetlana Maltseva <5437289+leo-ri@users.noreply.github.com>
2020-12-22 01:02:29 +0300

    fff

--- .github/actions/put-contents/entrypoint.sh
+++ .github/actions/put-contents/entrypoint.sh
@@ -4,6 +4,8 @@ export TODAY=$(date -u '+%Y-%m-%d')
 export MESSAGE="chore: regenerate $FILE_TO_COMMIT for $TODAY"
 export SHA=$(git rev-parse $DESTINATION_BRANCH:$FILE_TO_COMMIT)
 export CONTENT=$(base64 $FILE_TO_COMMIT)
+echo "$SHA"+
+echo "$DESTINATION_BRANCH:$FILE_TO_COMMIT"+
 if [ "$SHA" = "$DESTINATION_BRANCH:$FILE_TO_COMMIT" ]; then
     echo "not exist"
     gh api --method PUT /repos/leo-ri/test2/contents/$FILE_TO_COMMIT \
