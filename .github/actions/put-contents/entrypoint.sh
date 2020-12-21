4a998c51de51462611c6eae9367fbf084ac8a282
Svetlana Maltseva <5437289+leo-ri@users.noreply.github.com>
2020-12-22 00:14:15 +0300

    efsdf

--- .github/actions/put-contents/entrypoint.sh
+++ .github/actions/put-contents/entrypoint.sh
@@ -6,9 +6,17 @@ export TODAY=$(date -u '+%Y-%m-%d')
 export MESSAGE="chore: regenerate $FILE_TO_COMMIT for $TODAY"
 export SHA=$(git rev-parse $DESTINATION_BRANCH:$FILE_TO_COMMIT)
 export CONTENT=$(base64 -i $FILE_TO_COMMIT)
-gh api --method PUT /repos/leo-ri/test2/contents/$FILE_TO_COMMIT \
-    --field message="$MESSAGE" \
-    --field content="$CONTENT" \
-    --field encoding="base64" \
-    --field branch="$DESTINATION_BRANCH" \
-    --field sha="$SHA"
+if [ "$SHA" == "$DESTINATION_BRANCH:$FILE_TO_COMMIT"]; then
+    gh api --method PUT /repos/leo-ri/test2/contents/$FILE_TO_COMMIT \
+        --field message="$MESSAGE" \
+        --field content="$CONTENT" \
+        --field encoding="base64" \
+        --field branch="$DESTINATION_BRANCH" \
+else
+    gh api --method PUT /repos/leo-ri/test2/contents/$FILE_TO_COMMIT \
+        --field message="$MESSAGE" \
+        --field content="$CONTENT" \
+        --field encoding="base64" \
+        --field branch="$DESTINATION_BRANCH" \
+        --field sha="$SHA"
+fi
