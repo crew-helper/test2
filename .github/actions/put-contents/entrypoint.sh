c0c0b644d7a0687b35be773c89c0d252a5b66025
Svetlana Maltseva <5437289+leo-ri@users.noreply.github.com>
2020-12-28 14:00:40 +0300

    files

--- .github/actions/put-contents/entrypoint.sh
+++ .github/actions/put-contents/entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-export TODAY=$(date -u '+%Y-%m-%d')
-export MESSAGE="chore: regenerate $FILE_TO_COMMIT for $TODAY"
+#test
+mkdir -p delete/me
+$FILE_TO_COMMIT="delete/me/delete_me.txt"
+date >> $FILE_TO_COMMIT
+
+export MESSAGE="generated $FILE_TO_COMMIT"
 export SHA=$(git rev-parse $DESTINATION_BRANCH:$FILE_TO_COMMIT)
 export CONTENT=$(base64 $FILE_TO_COMMIT)
-echo "$SHA"+
-echo "$DESTINATION_BRANCH:$FILE_TO_COMMIT"+
+echo "$DESTINATION_BRANCH:$FILE_TO_COMMIT:$SHA"
+
 if [ "$SHA" = "$DESTINATION_BRANCH:$FILE_TO_COMMIT" ]; then
     echo "not exist"
     gh api --method PUT /repos/:owner/:repo/contents/$FILE_TO_COMMIT \
