481f1d18d2390e6ac68e8291521352f19bc84224
Svetlana Maltseva <5437289+leo-ri@users.noreply.github.com>
2020-12-29 22:53:12 +0300

    rename version

--- .github/actions/changelog/entrypoint.sh
+++ .github/actions/changelog/entrypoint.sh
@@ -6,7 +6,7 @@ echo "Commits: " > changelog.txt
 #find previos tag
 start_tag=$(git for-each-ref refs/tags/ --count=2 --sort=-version:refname --format='%(refname:short)' | awk 'NR==2')
 if [ -n "${start_tag}" ]; then
-    git log "${start_tag}...v${INPUT_VERSION}" --pretty=format:"- %s" >> changelog.txt
+    git log "${start_tag}...${INPUT_TAG}" --pretty=format:"- %s" >> changelog.txt
 else
     git log --pretty=format:"- %s" >> changelog.txt # first tag
 fi
