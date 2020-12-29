a0331fadca761a08c7b5466bb02268dd680fcb06
Svetlana Maltseva <5437289+leo-ri@users.noreply.github.com>
2020-12-29 22:58:42 +0300

    fix: changelog.md

--- .github/actions/changelog/entrypoint.sh
+++ .github/actions/changelog/entrypoint.sh
@@ -2,13 +2,14 @@
 
 # simple changelog: get all commits beetween 2 tags and put them into file
 
-echo "Commits: " > changelog.txt
+echo "Commits: " > changelog.md
 #find previos tag
 start_tag=$(git for-each-ref refs/tags/ --count=2 --sort=-version:refname --format='%(refname:short)' | awk 'NR==2')
 if [ -n "${start_tag}" ]; then
-    git log "${start_tag}...${INPUT_TAG}" --pretty=format:"- %s" >> changelog.txt
+    git log "${start_tag}...HEAD" --pretty=format:"- %s" >> changelog.md
 else
-    git log --pretty=format:"- %s" >> changelog.txt # first tag
+    # first tag
+    git log --pretty=format:"- %s" >> changelog.md
 fi
 
-cat changelog.txt
+cat changelog.md
