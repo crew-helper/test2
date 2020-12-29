b4944d9cc2e7d0159d4a97dbaa5aec129b62f0b0
Svetlana Maltseva <5437289+leo-ri@users.noreply.github.com>
2020-12-29 20:58:41 +0300

    cleanup

--- .github/actions/put-contents/entrypoint.sh
+++ .github/actions/put-contents/entrypoint.sh
@@ -2,22 +2,19 @@
 #commit file to the destination branch
 
 #test
-mkdir -p delete/me
-FILE_TO_COMMIT="delete/me/delete_me.txt"
-date >> $FILE_TO_COMMIT
-cat $FILE_TO_COMMIT
+# mkdir -p delete/me
+# FILE_TO_COMMIT="delete/me/delete_me.txt"
+# date >> $FILE_TO_COMMIT
+# cat $FILE_TO_COMMIT
 
 export MESSAGE="generated $FILE_TO_COMMIT"
 export SHA=$(git rev-parse $DESTINATION_BRANCH:$FILE_TO_COMMIT)
 export CONTENT=$(base64 $FILE_TO_COMMIT)
 echo "$DESTINATION_BRANCH:$FILE_TO_COMMIT:$SHA"
 
-# #TODO change API
-git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
-git config --local user.name "github-actions[bot]"
-
-date > t.txt
-# git checkout -b "$DESTINATION_BRANCH"
+# # #TODO change API
+# git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
+# git config --local user.name "github-actions[bot]"
 
 # Commit to the branch (exept main)
 if [ "$SHA" = "$DESTINATION_BRANCH:$FILE_TO_COMMIT" ]; then
